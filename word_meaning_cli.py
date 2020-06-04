6d60f5252833990c8d8742ec05b81eb48045563d
ironman777 <jonardonhazarika7@gmail.com>
2020-06-04 19:09:32 +0530

    commit

--- word_meaning_cli.py
+++ word_meaning_cli.py
@@ -5,9 +5,15 @@ def get_the_meaning(word):
     res = requests.get('https://api.dictionaryapi.dev/api/v2/entries/en/{}'.format(word)).text
     pattern = re.compile(r'"definition": ".+\."')
     meaning = re.findall(pattern,res)
-    return meaning[0]
+    if len(meaning) == 0:
+        print('Word not found!')
+    else:
+        return meaning[0]
 
 while True:
     word = str(input("Enter word: "))
-    print(get_the_meaning(word))
+    print(get_the_meaning(word),end='\n')
+    q = input(('enter q to quit\n')).strip()
+    if q.lower()=='q' or q.lower()=='quit':
+        quit()
     print('------------------------------------------------------------------')
